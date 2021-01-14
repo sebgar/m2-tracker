aa25f41457d01c60fcc74abf3bdc8712e4cf6ec5
sgarrouste <sgarrouste@datasolution.fr>
2021-01-14 11:10:49 +0100

    created_at and updated_at in save

--- Model/ResourceModel/Tag.php
+++ Model/ResourceModel/Tag.php
@@ -6,6 +6,7 @@ use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\Model\ResourceModel\Db\Context;
+use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Store\Model\StoreManagerInterface;
 use Sga\Tracker\Api\Data\TagInterface as ModelInterface;
 
@@ -14,17 +15,20 @@ class Tag extends AbstractDb
     protected $_storeManager;
     protected $_entityManager;
     protected $_metadataPool;
+    protected $_dateTime;
 
     public function __construct(
         Context $context,
         StoreManagerInterface $storeManager,
         EntityManager $entityManager,
         MetadataPool $metadataPool,
+        DateTime $dateTime,
         $connectionName = null
     ) {
         $this->_storeManager = $storeManager;
         $this->_entityManager = $entityManager;
         $this->_metadataPool = $metadataPool;
+        $this->_dateTime = $dateTime;
 
         parent::__construct($context, $connectionName);
     }
@@ -80,6 +84,11 @@ class Tag extends AbstractDb
 
     public function save(AbstractModel $object)
     {
+        if ($object->isObjectNew()) {
+            $object->setCreatedAt($this->_dateTime->gmtDate());
+        }
+        $object->setUpdatedAt($this->_dateTime->gmtDate());
+
         $this->packData($object);
         $this->_entityManager->save($object);
         return $this;
