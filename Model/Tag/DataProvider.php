b3102d219a9482e4fbd1e82e1bb3ada497ee1bc5
sgarrouste <sgarrouste@sga-dev.fr>
2022-01-12 08:27:24 +0100

    fix appel modifier

--- Model/Tag/DataProvider.php
+++ Model/Tag/DataProvider.php
@@ -13,6 +13,7 @@ class DataProvider extends \Magento\Ui\DataProvider\ModifierPoolDataProvider
     protected $collection;
     protected $dataPersistor;
     protected $loadedData;
+    protected $pool;
 
     public function __construct(
         $name,
@@ -22,24 +23,28 @@ class DataProvider extends \Magento\Ui\DataProvider\ModifierPoolDataProvider
         DataPersistorInterface $dataPersistor,
         array $meta = [],
         array $data = [],
-        PoolInterface $pool = null
+        PoolInterface $pool
     ) {
         $this->collection = $collectionFactory->create();
         $this->dataPersistor = $dataPersistor;
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data, $pool);
+        $this->pool = $pool;
     }
 
     public function getData()
     {
-        if (isset($this->loadedData)) {
-            return $this->loadedData;
-        }
         $items = $this->collection->getItems();
         foreach ($items as $item) {
             $item->getResource()->unpackData($item);
             $this->loadedData[$item->getId()] = $item->getData();
         }
 
+        // Call modifier
+        foreach ($this->pool->getModifiersInstances() as $modifier) {
+            $this->loadedData = $modifier->modifyData($this->loadedData);
+        }
+
+        // save data
         $data = $this->dataPersistor->get('tracker_tag');
         if (!empty($data)) {
             $model = $this->collection->getNewEmptyItem();
